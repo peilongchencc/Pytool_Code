94ee0cfa7bd3efede9f4041b4f217ca6349223b2
peilongchencc <peilongchencc@163.com>
2023-08-21 11:08:48 +0800

    更新代码

--- get_redis.py
+++ get_redis.py
@@ -1,9 +1,10 @@
 import redis
+import pickle
 
 # 连接到Redis
 r = redis.Redis(host='localhost', port=6379)
 
 # 获取存储在哈希中的全部键值对
-result = r.get("my_str")                        # b'Hello, world!'
-decoded_result = result.decode()                # 等同于 result.decode("utf-8")；
-print(decoded_result)                           # Hello, world!
+result = r.get("my_dict")
+result = pickle.loads(result)
+print(result)
--- to_redis.py
+++ to_redis.py
@@ -1,11 +1,12 @@
-# 将dict存入 Redis；
-
 import redis
+import pickle
 
 # 连接到Redis
 r = redis.Redis(host='localhost', port=6379)
 
-data = "Hello, world!"
-
+data = {"key1": "value1",
+        "key2": "value2",
+        "key3": "value3"}
+data = pickle.dumps(data)
 # 使用hmset命令将字典存储为一个哈希
-r.set("my_str", data)
+r.set("my_dict", data)
