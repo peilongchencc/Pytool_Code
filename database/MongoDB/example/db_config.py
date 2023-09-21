ddfc0a34e5596f8872de66787f481b6c833af231
peilongchencc <peilongchencc@163.com>
2023-09-21 17:04:10 +0800

    更新代码

--- database/MongoDB/example/db_config.py
+++ database/MongoDB/example/db_config.py
@@ -1,8 +1,8 @@
 Mongodb_Server_Config ={
     "mongodb_host": "localhost",
     "mongodb_port": 27017,
-    "mongodb_database": "test_database",
-    "mongodb_collection": "test_collection"
+    "mongodb_database": "test_database",        # 数据库
+    "mongodb_collection": "test_collection"     # 集合
 }
 
 Mongodb_Id_Path = './mongo_id.json'
