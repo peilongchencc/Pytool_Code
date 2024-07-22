af65fc010d21a9dd2ada6775d434f73d0c63afb2
peilongchencc <peilongchencc@163.com>
2024-07-22 15:49:43 +0800

    添加markdown警示框教程

--- database_tutorial/MongoDB/example/db_config.py
+++
@@ -1,8 +0,0 @@
-Mongodb_Server_Config ={
-    "mongodb_host": "localhost",
-    "mongodb_port": 27017,
-    "mongodb_database": "test_database",        # 数据库
-    "mongodb_collection": "test_collection"     # 集合
-}
-
-Mongodb_Id_Path = './mongo_id.json'
--- database_tutorial/README.md
+++
@@ -1,3 +0,0 @@
-# database_tutorial
-
-基于python,介绍常见数据库(MySQL、Redis、Neo4j)的安装与使用。<br>
